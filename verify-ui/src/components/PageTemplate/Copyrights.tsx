6f8f2415cb49b91d4caf1cd37200161e505b151a
srikanth716 <srikanthsri7447@gmail.com>
2026-07-16 10:05:10 +0530

    [INJIVER-2095] fix: footer cropping the error message on offline page
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>

--- verify-ui/src/components/PageTemplate/Copyrights.tsx
+++ verify-ui/src/components/PageTemplate/Copyrights.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { useLocation } from "react-router-dom";
+import { Pages } from "../../utils/config";
 
 function Copyrights(props: any) {
     const {t} = useTranslation("CopyRight");
+    const { pathname } = useLocation();
+    const isOfflinePage = pathname === Pages.Offline;
 
     return (
-        <div className="fixed grid bottom-0 w-[100vw] lg:w-[49vw] content-center justify-center bg-white">
+        <div className={`grid w-[100vw] lg:w-[49vw] content-center justify-center bg-white ${isOfflinePage ? "" : "fixed bottom-0"}`}>
             <div className="xs:w-[90vw] lg:w-[40vw] mx-auto border-b-[1px] border-b-copyRightsBorder opacity-20"/>
             <p id="copyrights-content" className="py-4 px-0 w-[100%] text-center text-normalTextSize font-normal text-copyRightsText">
                 {t('content')}
