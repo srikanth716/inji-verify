3a9e9bb5345abb099a5590f50e787125eaba23eb
Srikanth Kola <97477121+srikanth716@users.noreply.github.com>
2026-07-16 12:00:01 +0530

    [INJIVER-2095] fix: footer cropping the error message on offline page (#2149)
    * [INJIVER-2095] fix: footer cropping the error message on offline page
    
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>
    
    * [INJIVER-2095] fix: Remove unused props parameter
    
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>
    
    ---------
    
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>

--- verify-ui/src/__tests__/components/Home/VerificationProgressTracker/Copyrights.spec.tsx
+++ verify-ui/src/__tests__/components/Home/VerificationProgressTracker/Copyrights.spec.tsx
@@ -1,13 +1,40 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import Copyrights from "../../../../components/PageTemplate/Copyrights";
+import { Pages } from "../../../../utils/config";
 
 describe("Copyrights", () => {
   test("renders copyrights content element", () => {
-    const { container } = render(<Copyrights />);
+    const { container } = render(
+      <MemoryRouter>
+        <Copyrights />
+      </MemoryRouter>
+    );
 
     expect(
       container.querySelector("#copyrights-content")
     ).toBeInTheDocument();
+  });
+
+  test("uses fixed positioning on non-offline pages", () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={[Pages.Home]}>
+        <Copyrights />
+      </MemoryRouter>
+    );
+
+    expect(container.firstChild).toHaveClass("fixed", "bottom-0");
+  });
+
+  test("uses normal document flow on offline page", () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={[Pages.Offline]}>
+        <Copyrights />
+      </MemoryRouter>
+    );
+
+    expect(container.firstChild).not.toHaveClass("fixed");
+    expect(container.firstChild).not.toHaveClass("bottom-0");
   });
 });
